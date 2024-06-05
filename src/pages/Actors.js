8119b2865de470de14472a03bcdd622ce689bef2
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-06-05 10:57:34 +0300

    added routes to react app

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then((r) => r.json())
+      .then((data) => setActors(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const actorsList = actors.map((actor) => {
+    return (
+      <article key={actor.id}>
+        <h2>{actor.name}</h2>
+        <ul>
+          {actor.movies.map((movie, index) => {
+            return <li key={index}>{movie}</li>;
+          })}
+        </ul>
+      </article>
+    );
+  });
+
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
+        <h1>Actors Page</h1>
       </header>
-      <main>
-        {/* Actor info here! */}
-      </main>
+      <main>{/* Actor info here! */}</main>
+      <main>{actorsList}</main>
     </>
   );
-};
+}
 
 export default Actors;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const movieList = movies.map((movie) => {
+    return <MovieCard key={movie.id} title={movie.title} id={movie.id}/>;
+  });
+
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
+        <h1>Home Page</h1>
       </header>
       <main>
         {/* Info goes here! */}
+        {/*----- MOVIE LIST -----*/}
+        {movieList}
       </main>
     </>
   );
-};
+}
 
 export default Home;
